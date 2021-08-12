f00d790088ba423fa5dd919b99797411aff5d925
pedrolucaspalma <pedrolucaspalma@gmail.com>
2021-08-12 15:32:09 -0300

    adicionar listagem de cursos

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+//pega as informações dos cursos
 function fetchFormacoesInfo(){
     fetch('https://www.alura.com.br/api/formacoes')
         .then(response => response.json())
-        .then(todasAsFormacoes => console.log(todasAsFormacoes));
+        //.then(todasAsFormacoes => console.log(todasAsFormacoes));
+        .then(function(todasAsFormacoes){
+            console.log(todasAsFormacoes);
+            todasAsFormacoes.forEach(function(formacao){
+                renderFormacao(formacao);
+                // console.log(this);
+            });
+        })
+}
+
+//renderiza na tela por DOM
+function renderFormacao(formacao){
+    let containerDeFormacoes = document.getElementById('alura-container');
+
+    let formacaoContainer = document.createElement("div");
+    formacaoContainer.className = "formaçao";
+
+    let nomeDaFormacao = document.createElement('h4');
+    nomeDaFormacao.innerText = formacao.title;
+
+    formacaoContainer.append(nomeDaFormacao);
+    containerDeFormacoes.appendChild(formacaoContainer);
 }
 
 fetchFormacoesInfo();
